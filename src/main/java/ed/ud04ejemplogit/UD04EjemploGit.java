1b0b682ab7d7c4ca561347bd92283417783cd6a2
amaiur <aberasategi@birt.eus>
2026-05-11 16:58:07 +0200

    Versión 0. Primer commit del proyecto

---
+++ src/main/java/ed/ud04ejemplogit/UD04EjemploGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ed.ud04ejemplogit;
+
+/**
+ *
+ * @author amaiu
+ */
+public class UD04EjemploGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
