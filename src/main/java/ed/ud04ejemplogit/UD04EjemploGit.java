faf423fcd4274a0e634d927b1efc2872b33ddd22
Amaiur82 <aberasategi@birt.eus>
2026-05-12 16:33:45 +0200

    Version 5 cambios desde GitHub

--- src/main/java/ed/ud04ejemplogit/UD04EjemploGit.java
+++ src/main/java/ed/ud04ejemplogit/UD04EjemploGit.java
@@ -4,12 +4,12 @@ package ed.ud04ejemplogit;
 /**
  * Descripcion de la clase esto es para SSH prueba 2 SSH
  *
+ * pruebas de GitHub otra persona
  * @author amaiur
  */
 public class UD04EjemploGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         System.out.println("Holis, caracolis");
     }
 }
